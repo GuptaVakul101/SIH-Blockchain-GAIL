18f6954360c702b5eb198e803ff198ac8e93097c
niyati2k <niyati18@iitg.ac.in>
2020-07-19 01:16:23 +0530

    Created url getProject to check  getProject function in chaincode

--- nodeserver/routes/gail/projects.js
+++ nodeserver/routes/gail/projects.js
@@ -73,8 +73,11 @@ router.post('/createProject', async function(req, res, next) {
             discovery: { enabled: true, asLocalhost: true } });
             const network = await gateway.getNetwork('channelgg');
             const contract = network.getContract('gail', 'Project');
+            const incrementNumProjects = await contract.evaluateTransaction('incrementNumProjects');
+            const numProjects = await contract.evaluateTransaction('getNumProjects');
 
             const project = await contract.evaluateTransaction('createProject', req.body.username, req.body.id, req.body.title, req.body.description);
+            //const backproj = await contract.evaluateTransaction('getProject',req.body.id);
             const jsonObj = JSON.parse(project.toString());
             console.log("Project Creation: " + jsonObj.success);
 
@@ -82,11 +85,83 @@ router.post('/createProject', async function(req, res, next) {
 
             res.json({
                 success: true,
-                message: 'Successfully created a new Project' 
+                message: 'Successfully created a new Project ' 
             });
 
         }
     }
-});
 
+    
+});
+router.post('/getProject', async function(req, res, next) {
+	const ccpPath = path.resolve(__dirname, '..', '..', '..', 'fabric', 'test-network', 'organizations',
+    'peerOrganizations', 'gail.example.com', 'connection-gail.json');
+    const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+    // Create a new file system based wallet for managing identities.
+    const walletPath = path.join(__dirname, 'wallet');
+    const wallet = await Wallets.newFileSystemWallet(walletPath);
+
+    // Check to see if we've already enrolled the user.
+    const identity = await wallet.get(req.body.username);
+    if (!identity) {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            success: false,
+            message: 'You dont have permission to access this page!!' 
+        });
+    }   
+    else{
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: req.body.username,
+            discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('channelgg');
+
+        // Get the contract from the network.
+        const contract = network.getContract('gail', 'User');
+
+        const user = await contract.evaluateTransaction('getUser', req.body.username, req.body.password);
+        const jsonObj = JSON.parse(user.toString());
+        console.log(jsonObj.success);
+        await gateway.disconnect();
+
+        if(jsonObj.success == "false") {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+                success: false,
+                message: 'You dont have permission to access this page!!' 
+            });
+        } 
+        else {
+            //correct username and password, proceed further to create new project
+            const gateway = new Gateway();
+            await gateway.connect(ccp, { wallet, identity: req.body.username,
+            discovery: { enabled: true, asLocalhost: true } });
+            const network = await gateway.getNetwork('channelgg');
+            const contract = network.getContract('gail', 'Project');
+            //const incrementNumProjects = await contract.evaluateTransaction('incrementNumProjects');
+            //const numProjects = await contract.evaluateTransaction('getNumProjects');
+
+            //const project = await contract.evaluateTransaction('createProject', req.body.username, req.body.id, req.body.title, req.body.description);
+            const backproj = await contract.evaluateTransaction('getProject',req.body.id);
+            const jsonObj = JSON.parse(backproj.toString());
+            console.log("Project Creation: " + jsonObj.success);
+
+            await gateway.disconnect();
+
+            res.json({
+                success: true,
+                message: 'Successfully got the project ' + backproj
+            });
+
+        }
+    }
+
+    
+});
 module.exports = router;
