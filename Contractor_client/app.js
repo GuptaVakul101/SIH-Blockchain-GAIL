831799392eb0bc1d95b04f8924f0c313fa283594
LavishGulati <lavish00gulati@gmail.com>
2020-07-29 10:56:12 +0530

    Added Contractor profile page and initializes edit profile

--- Contractor_client/app.js
+++ Contractor_client/app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var indexRoutes = require("./routes/index");
 var projectRoutes = require("./routes/projects");
-var cookieParser = require('cookie-parser');
-
+var userRoutes = require("./routes/profile");
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + "/public"));
 
 app.use("/", indexRoutes);
 app.use("/", projectRoutes);
-
+app.use("/profile/", userRoutes);
 
 app.listen(4200,function(){
     console.log("Server started at port 4200");
--- nodeserver/routes/contractors/users.js
+++ nodeserver/routes/contractors/users.js
@@ -210,24 +210,23 @@ router.post('/signup', async function(req, res, next){
     }
 });
 
-router.post('/:username', async function(req, res, next) {
-    const dictionary=JSON.parse(fs.readFileSync(path.resolve(__dirname,'dictionaryRev.json'), 'utf8'));
-    var contractorUsername = req.params.username.toString();
-    if(dictionary.hasOwnProperty(contractorUsername)){
-        var contractorDetails=dictionary[contractorUsername];
+router.post('/profile', async function(req, res, next) {
+    const contractor = await utility.getUser(req.body.username, req.body.password);
+    if(contractor == null){
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            success: false,
+            message: 'No such contractor ID exists'
+        });
+    }
+    else{
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json({
             success: true,
             message: 'Successfully get contractor details',
-            object: contractorDetails
-        });
-    }else{
-        res.statusCode = 400;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            success: false,
-            message: 'No such contractor id exists'
+            object: contractor
         });
     }
 });
