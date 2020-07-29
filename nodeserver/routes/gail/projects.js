cc276079e9a32ec76f6ec285e5a147f78d4c2490
Chirag Gupta <cgupta3131@gmail.com>
2020-07-29 16:45:36 +0530

    Able to edit Profile

--- nodeserver/routes/gail/projects.js
+++ nodeserver/routes/gail/projects.js
@@ -32,13 +32,10 @@ router.post('/createProject', async function (req, res, next) {
         'peerOrganizations', 'gail.example.com', 'connection-gail.json');
     const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
 
-    // Create a new file system based wallet for managing identities.
     const walletPath = path.join(__dirname, 'wallet');
     const wallet = await Wallets.newFileSystemWallet(walletPath);
-
-    // Check to see if we've already enrolled the user.
-    console.log("Username: " + req.body.username);
     const identity = await wallet.get(req.body.username);
+
     if (!identity) {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'application/json');
@@ -48,17 +45,14 @@ router.post('/createProject', async function (req, res, next) {
         });
     }
     else {
-        // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
         await gateway.connect(ccp, {
             wallet, identity: req.body.username,
             discovery: { enabled: true, asLocalhost: true }
         });
 
-        // Get the network (channel) our contract is deployed to.
         const network = await gateway.getNetwork('channelgg');
 
-        // Get the contract from the network.
         const contract = network.getContract('gail', 'User');
 
         const user = await contract.evaluateTransaction('getUser', req.body.username, req.body.password);
